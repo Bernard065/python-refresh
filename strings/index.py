a97c3290e3d6873512d6617c80654ce1d23b1e46
Bernard065 <bebenibernard@gmail.com>
2024-10-21 11:38:27 +0300

    String methods

---
+++ strings/index.py
@@ -0,0 +1,8 @@
+# Split method
+text = 'apple, banana, cherry'
+
+print(text.split(", "))
+
+# Join elements of an iterable (e.g., a list) into a string with a specified separator.
+fruits = ['apple', 'banana', 'cherry']
+print(', '.join(fruits))
