07b73a7f99045a1fbc155e0ca7a9715c1bcc7ef4
Bernard065 <bebenibernard@gmail.com>
2024-10-24 17:24:15 +0300

    random library

---
+++ libraries/random_choice.py
@@ -0,0 +1,12 @@
+# Returns a randomly selected item from the given sequence.
+
+# import random
+
+# coin = random.choice(["tails", "heads"])
+# print(coin)
+
+from random import choice
+
+coin = choice(["tails", "heads"])
+print(coin)
+
---
+++ libraries/random_randiint.py
@@ -0,0 +1,5 @@
+# generate a random integer within a specified range
+import random
+
+number = random.randint(1, 10)
+print(number)
